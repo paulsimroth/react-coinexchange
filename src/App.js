632cdc84af485b30aa0af0f7fdf8bf9cf583ca86
paulsimroth <paul.simroth@hotmail.com>
2022-05-17 15:43:29 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from "./logo.svg";
+import Coin from "./components/Coin/Coin";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} alt="React Logo" className="App-logo"/>
+        <h1 className="App-title">
+          Coin Exchange 
+        </h1>
       </header>
+      <table className="Coin-table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Ticker</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <Coin name="Bitcoin" ticker="BTC" price={28000.00} />
+          <Coin name="Ethereum" ticker="ETH" price={2000.00} />
+          <Coin name="Tether" ticker="USDT" price={1.00}/>
+          <Coin name="Cardano" ticker="ADA" price={0.5} />
+        </tbody>
+      </table>
     </div>
   );
 }
