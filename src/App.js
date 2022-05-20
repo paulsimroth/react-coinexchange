ac980fff5cd5f662952ad42eac931fdc60648b04
paulsimroth <paul.simroth@hotmail.com>
2022-05-20 14:22:59 +0200

    componentization ad prop drilling

--- src/App.js
+++ src/App.js
@@ -1,35 +1,60 @@
-import './App.css';
-import logo from "./logo.svg";
-import Coin from "./components/Coin/Coin";
+import CoinList from "./components/CoinList/CoinList";
+import ExchangeHeader from "./components/ExchangeHeader/ExchangeHeader";
 import AccountBalance from "./components/AccountBalance/AccountBalance";
+import React from 'react';
+import styled from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="React Logo" className="App-logo"/>
-        <h1 className="App-title">
-          Coin Exchange 
-        </h1>
-      </header>
-      <AccountBalance amount={10000}/>
-      <table className="Coin-table">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Ticker</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          <Coin name="Bitcoin" ticker="BTC" price={28000.00} />
-          <Coin name="Ethereum" ticker="ETH" price={2000.00} />
-          <Coin name="Tether" ticker="USDT" price={1.00}/>
-          <Coin name="Cardano" ticker="ADA" price={0.5} />
-        </tbody>
-      </table>
-    </div>
+const Div = styled.div`
+  text-align: center;
+  background-color: rgb(60, 111, 168);
+  color: #cccccc;
+`;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      balance: 10000,
+      coinData: [
+        {
+          name: "Bitcoin",
+          ticker: "BTC",
+          price: 28000,
+        },
+        {
+          name: "Ethereum",
+          ticker: "ETH",
+          price: 2000,
+        },
+        {
+          name: "Tether",
+          ticker: "USDT",
+          price: 1,
+        },
+        {
+          name: "Cardano",
+          ticker: "ADA",
+          price: 0.5,
+        },
+        {
+          name: "BNB",
+          ticker: "BNB",
+          price: 305.0,
+        }
+      ]
+    }
+  }
+
+  render() {
+      return (
+    <Div className="App">
+      <ExchangeHeader/>
+      <AccountBalance amount={this.state.balance}/>
+      <CoinList coinData = {this.state.coinData} />
+    </Div>
   );
+  }
+
 }
 
 export default App;
---
+++ src/components/CoinList/CoinList.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Coin from "../Coin/Coin";
+import styled from "styled-components";
+
+const Table = styled.table`
+    margin: 50px auto 50px auto;
+    display: inline-block;
+    font-size: 1.4rem;
+`;
+
+export default class CoinList extends Component {
+  render() {
+    return (
+        <Table>
+        <thead>f
+          <tr>
+            <th>Name</th>
+            <th>Ticker</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            this.props.coinData.map( ({name, ticker, value}) => 
+              <Coin key={value.ticker} name={name} ticker={ticker} price={price}/>)
+          }
+        </tbody>
+      </Table>
+    )
+  }
+}
